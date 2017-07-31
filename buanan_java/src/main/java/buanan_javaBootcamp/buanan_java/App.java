011d16cac43971064052062d97adf0c9c4fc526d
BUANAN <BUANAN@ZHA-ITA102-W7.corp.oocl.com>
2017-07-31 15:54:19 +0800

    master branch

---
+++ buanan_java/src/main/java/buanan_javaBootcamp/buanan_java/App.java
@@ -0,0 +1,13 @@
+package buanan_javaBootcamp.buanan_java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
